f7b9a7b46a7720fa063848aaf1118e0bbeb6c97f
CrisStefani <you@example.com>
2020-12-12 18:20:38 -0300

    HTML

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const express = require(`express`);
-const path = require(`path`);
-
-
+const express = require('express');
 const app = express();
 
-const publicPath =path.resolve(__dirname, `/public`);
  
-app.use( express.static(publicPath) );
+
+app.use(express.static('public'));
 
 
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
 
-app.listen(3000, () => {
-        console.log("Servidor corriendo en el puerto 3000");
- });
 
-
-app.get (`/`, (req, res) => {
-    res.sendFile(path.resolve(__dirname, `./views/home.html`));
+app.get('/', (req,res)=>{
+    
+    res.sendFile(__dirname + '/views/home.html');
+    
 });
